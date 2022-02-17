34e6461ee48f036ff8935bad901ce3050506eb6c
Techbenq <97280467+Techbenq@users.noreply.github.com>
2022-02-17 16:26:48 +0200

    Made lightning little better

--- Survive!/Assets/Scripts/DayNightCycle.cs
+++ Survive!/Assets/Scripts/DayNightCycle.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
+    public Gradient ambient, fog, directional;
     public bool isDay = true;
     public float rotateSpeed;
     public float dayLength;
 
     private float currentTime;
     private float dayProcent;
+
+    private Light directionalLight;
+    private void Start()
+    {
+        directionalLight = GetComponent<Light>();
+    }
+
     private void Update()
     {
         currentTime += Time.deltaTime * rotateSpeed;
@@ -20,6 +28,15 @@ public class DayNightCycle : MonoBehaviour
             isDay = false;
         else isDay = true;
 
+        Debug.Log(dayProcent);
         transform.eulerAngles = new Vector3(360 * dayProcent, 0);
+        UpdateLightning();
+    }
+
+    private void UpdateLightning()
+    {
+        RenderSettings.ambientLight = ambient.Evaluate(dayProcent);
+        RenderSettings.fogColor = fog.Evaluate(dayProcent);
+        directionalLight.color = directional.Evaluate(dayProcent);
     }
 }
