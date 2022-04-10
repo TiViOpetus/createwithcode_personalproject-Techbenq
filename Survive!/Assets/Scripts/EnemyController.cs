b65c1406461c00461341a5521d37598276c10f4e
Techbenq <97280467+Techbenq@users.noreply.github.com>
2022-04-10 23:10:25 +0300

    Added animations to enemy

--- Survive!/Assets/Scripts/EnemyController.cs
+++ Survive!/Assets/Scripts/EnemyController.cs
@@ -6,17 +6,52 @@ using UnityEngine.AI;
 public class EnemyController : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Animator anim;
     public float attackDistance;
     public Transform player;
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.Find("Human").GetComponent<Transform>();
     }
 
     private void Update()
     {
-        agent.SetDestination(player.position);
+        float distance;
+        distance = Vector3.Distance(transform.position, player.position);
+
+        if(distance <= attackDistance)
+        {
+            FaceTarget();
+            Attack();
+        }
+        else
+        {
+            agent.SetDestination(player.position);
+        }
+
+        if(agent.velocity.magnitude > 0.2f)
+        {
+            anim.SetFloat("Speed", 1);
+        }
+        else
+        {
+            anim.SetFloat("Speed", 0);
+        }
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = player.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attacking");
+        anim.SetTrigger("Attack");
     }
 }
