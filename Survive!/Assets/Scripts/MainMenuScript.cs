ef736b6ecd242c3c2569e50408ccb19f023de071
Techbenq <97280467+Techbenq@users.noreply.github.com>
2022-04-28 13:53:11 +0300

    Added main menu

--- Survive!/Assets/Scripts/MainMenuScript.cs
+++ Survive!/Assets/Scripts/MainMenuScript.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    
+    public CanvasGroup howToPlay;
+
     public void ChangeSeed(string seedText)
     {
-        if(seedText != null)
+        if(seedText.Length > 0)
         {
             int seed = int.Parse(seedText);
             if (seed > 0)
@@ -24,6 +26,23 @@ public class MainMenuScript : MonoBehaviour
         else
         {
             WorldGenerationV2.randomSeed = true;
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowHowToPlay()
+    {
+        if(howToPlay.alpha == 0)
+        {
+            howToPlay.alpha = 1;
+        }
+        else
+        {
+            howToPlay.alpha = 0;
         }
     }
 }
